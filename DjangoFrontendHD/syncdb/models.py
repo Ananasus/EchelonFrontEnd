b8b726b51e8960c5ea5f48947dbb8c02f40b177d
Herry Pattar <fake_empire@mail.ru>
2015-07-17 03:14:13 +0300

    Added previously untracked files

---
+++ DjangoFrontendHD/syncdb/models.py
@@ -0,0 +1,22 @@
+"""
+Definition of models.
+"""
+from django.db import models
+import json
+# Create your models here.
+class Event(models.Model):
+    uid = models.BigIntegerField,
+    sid = models.BigIntegerField,
+    alert_class = models.CharField,
+    description = models.TextField,
+    def __str__(self):
+        return "[ Security Event: UID: "+self.uid.__str__()+" SID: "+self.sid.__str__()+" Alert Class: "+self.alert_class+" Description: "+self.description+" ]"
+    def __init__(self, uid, sid, alert_class, description):
+        self.alert_class = alert_class
+        self.uid = uid
+        self.description = description
+        self.sid = sid
+    def serialize(self):
+        return self.__dict__
+    def deserialize(self, data):
+        self.__dict__ = json.loads(data)
---
+++ DjangoFrontendHD/syncdb/redissync.py
@@ -0,0 +1,67 @@
+## Module for syncing with Redis database
+import redis
+import random
+from syncdb.models import Event
+
+
+def connect_to_redis():
+    password = '1233'
+    port = 11281 
+    db_no = 0
+    redis_connection = redis.StrictRedis('pub-redis-11281.us-east-1-4.5.ec2.garantiadata.com', port, db_no, password, 90, 10, True)
+    return redis_connection
+
+
+def data_gen(number_of:int,str_prefix:str):
+    r = connect_to_redis()
+    name = str_prefix+random.randint(0,1000000).__str__()
+    array = []
+    for i in range(0,number_of):
+        array.append(Event(random.getrandbits(128),random.getrandbits(128),'a',"Just some random text #"+random.randint(0,2*number_of).__str__()).serialize())
+    result = r.rpush(name, *array)
+    if(result == number_of):
+        print('OK. SET IS FROM')
+    else:
+        print('FAIL. SET IS FROM')
+        print(result)
+        return 1
+    return 0
+
+
+def request_most_recent_data(last_known):
+    r = connect_to_redis()
+    scan_result = r.scan(0, "events_recent*", 100)
+    if(len(scan_result[1])>0):
+        result = scan_result[1]
+        str(result[1:])
+        result.sort(reverse=True)
+        position = -1
+        recent_keys = []
+        recent = []
+        try:
+            position = result.index(last_known)
+        except:
+            pass
+        if(position==-1):
+            recent_keys = result
+        else:
+            recent_keys = result[0:position]
+        for key in recent_keys:
+            recent += r.lrange(key, 0, -1)
+    return [ recent_keys[0] , recent ]
+
+
+
+
+def request_data():
+    r = connect_to_redis()
+    #parse arguments
+
+    
+    result = r.get("ololo")
+    if(result == True):
+        print('OK. SET IS FROM')
+    else:
+        print('ConnectionError set failed')
+        return 1
+    return 0
